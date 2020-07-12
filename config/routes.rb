1ac2f875c804356272447dd460dea914fa50dbcd
paydaylight <maxat235@gmail.com>
2020-07-12 10:57:33 +0600

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, default: :json do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       scope :medical_procedures do
         get "search" => "medical_procedures#search"
