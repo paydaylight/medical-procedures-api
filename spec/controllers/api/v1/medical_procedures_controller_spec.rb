28b97d63417cbb7c94cb141c6719d1f27864360f
paydaylight <maxat235@gmail.com>
2020-07-12 12:39:46 +0600

    test refactor

--- spec/controllers/api/v1/medical_procedures_controller_spec.rb
+++ spec/controllers/api/v1/medical_procedures_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Api::V1::MedicalProceduresController, :type => :controller do
   describe "validation of GET search" do
     it "has a 400 status code when 'q' empty" do
-      get :search, params: {p: 1}
+      get :search, params: {p: "1"}
       expect(response.status).to eq(400)
       end
 
@@ -18,7 +18,7 @@ RSpec.describe Api::V1::MedicalProceduresController, :type => :controller do
     end
 
     it "has a 200 status code when 'p' is an integer" do
-      get :search, params: {q: "test", p: 1}
+      get :search, params: {q: "test", p: "1"}
       expect(response.status).to eq(400)
     end
   end
