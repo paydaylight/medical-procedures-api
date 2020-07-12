e41f00ae40bc4d0950659dae7747911caaa55cd3
paydaylight <maxat235@gmail.com>
2020-07-12 11:15:21 +0600

    strict search tests

---
+++ spec/controllers/api/v1/medical_procedures_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::MedicalProceduresController, :type => :controller do
+  describe "validation of GET search" do
+    it "has a 400 status code when 'q' empty" do
+      get :search, params: {p: 1}
+      expect(response.status).to eq(400)
+      end
+
+    it "has a 400 status code when 'p' empty" do
+      get :search, params: {q: "test"}
+      expect(response.status).to eq(400)
+    end
+
+    it "has a 400 status code when 'p' is not an integer" do
+      get :search, params: {q: "test", p: "text"}
+      expect(response.status).to eq(400)
+    end
+
+    it "has a 200 status code when 'p' is an integer" do
+      get :search, params: {q: "test", p: 1}
+      expect(response.status).to eq(400)
+    end
+  end
+
+
+
+end
