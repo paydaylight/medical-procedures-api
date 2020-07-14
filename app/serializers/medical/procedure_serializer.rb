c2e3f01e93a409c8c419ff8b0cf6e0f7bb8548f8
paydaylight <maxat235@gmail.com>
2020-07-14 16:37:50 +0600

    added limit to serializer

--- app/serializers/medical/procedure_serializer.rb
+++ app/serializers/medical/procedure_serializer.rb
@@ -2,6 +2,6 @@ class Medical::ProcedureSerializer < ActiveModel::Serializer
   attributes :id, :name, :linked
 
   def linked
-    ActiveModelSerializers::SerializableResource.new(object.children, each_serializer: Medical::ProcedureSerializer).as_json
+    ActiveModelSerializers::SerializableResource.new(object.children.limit(5), each_serializer: Medical::ProcedureSerializer).as_json
   end
 end
